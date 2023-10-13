bc7add7fc1b9765ce446e09b1c662ba3829e6a10
bekhungyi <bekhungyi22@gmail.com>
2023-10-13 15:40:28 +0800

    e1

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bhung-yi <bhung-yi@student.42kl.edu.my>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/13 15:35:26 by bhung-yi          #+#    #+#             */
+/*   Updated: 2023/10/13 15:35:27 by bhung-yi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	int		i;
+	int		j;
+
+	j = -1;
+	if (argc < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	while (argc > 1 && argv && argv[++j])
+	{
+		i = -1;
+		while (j > 0 && argv[j][++i])
+			std::cout << (char)std::toupper(argv[j][i]);
+	}
+	std::cout << std::endl;
+	return (0);
+}
