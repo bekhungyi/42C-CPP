83ec357fd7805169475fb0c1a62b480561e1c602
Bek Hung Yi <bhung-yi@u80z03s07.42kl.edu.my>
2023-11-07 16:05:18 +0800

    upd cpp04

--- cpp04/ex03/srcs/main.cpp
+++ cpp04/ex03/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: bhung-yi <bhung-yi@student.42kl.edu.my>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 02:47:42 by bhung-yi          #+#    #+#             */
-/*   Updated: 2023/10/30 18:19:22 by bhung-yi         ###   ########.fr       */
+/*   Updated: 2023/11/07 16:03:51 by bhung-yi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,23 +41,3 @@ int main()
     delete bob;
     return 0;
 }
-
-// int main()
-// {
-//     IMateriaSource* src = new MateriaSource();
-//     src->learnMateria(new Ice());
-//     src->learnMateria(new Cure());
-//     ICharacter* me = new Character("me");
-//     AMateria* tmp;
-//     tmp = src->createMateria("ice");
-//     me->equip(tmp);
-//     tmp = src->createMateria("cure");
-//     me->equip(tmp);
-//     ICharacter* bob = new Character("bob");
-//     me->use(0, *bob);
-//     me->use(1, *bob);
-//     delete bob;
-//     delete me;
-//     delete src;
-//     return 0;
-// }
